1e69d10f96467f41c5234ecde4bc0b6fa5213523
MD. SHIHAB KHAN POLASH <97795811+polashkhanpk@users.noreply.github.com>
2025-12-05 09:00:22 +0600

    complete cancel.php

--- cancel.php
+++ cancel.php
@@ -1,1 +1,20 @@
+<?php
 
+    include 'Database/connection.php';
+
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM appointments WHERE id = '$id'";
+    
+    $run = mysqli_query($conn, $sql);
+
+    if(!$run){
+        echo 'delete operation failed!';
+    } else{
+        echo "<script>
+            alert('Apointment Cancel successfully!');
+            window.location.href='apointmentlist.php';
+          </script>";
+    }
+
+?>
