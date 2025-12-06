695c80cb7549ba30104dcd19161677977531a7b0
Ismail Hossain <117060583+ismail-hossain1@users.noreply.github.com>
2025-12-06 13:52:55 +0600

    Complete CSV export functionality for patient list
    Export patient data to CSV format with headers.

--- patient/export_csv.php
+++ patient/export_csv.php
@@ -1,1 +1,21 @@
+<?php
+include '../Database/connection.php';
 
+header('Content-Type: text/csv');
+header('Content-Disposition: attachment; filename="patient_list.csv"');
+
+$output = fopen("php://output", "w");
+
+
+fputcsv($output, ['ID', 'Patient ID', 'First Name', 'Last Name', 'Email', 'Phone no', 'Mobile no', 'Blood Group', 'Sex', 'DOB', 'Address', 'Status']);
+
+$query = "SELECT * FROM patients";
+$result = mysqli_query($conn, $query);
+
+while ($row = mysqli_fetch_assoc($result)) {
+    fputcsv($output, $row);
+}
+
+fclose($output);
+exit;
+?>
