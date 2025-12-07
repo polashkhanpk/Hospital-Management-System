9338f5393f234548e63c25656534236f47c75541
Bayajid35 <bustami22205101235@diu.edu.bd>
2025-12-07 20:52:31 +0600

    Update edit_doctor.php

--- doctor/edit_doctor.php
+++ doctor/edit_doctor.php
@@ -13,3 +13,40 @@ if (!$result || mysqli_num_rows($result) != 1) {
 
 $doctor = mysqli_fetch_assoc($result);
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Edit Doctor</title>
+    <link rel="stylesheet" href="edit_style.css">
+</head>
+<body>
+
+<div class="edit-container">
+    <h2>Edit Doctor</h2>
+    <form action="update_doctor.php" method="post">
+        <input type="hidden" name="id" value="<?= $doctor['id'] ?>">
+
+        <div class="form-group">
+            <label>First Name</label>
+            <input type="text" name="first_name" value="<?= $doctor['first_name'] ?>">
+        </div>
+
+        <div class="form-group">
+            <label>Last Name</label>
+            <input type="text" name="last_name" value="<?= $doctor['last_name'] ?>">
+        </div>
+
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" name="email" value="<?= $doctor['email'] ?>">
+        </div>
+
+        <div class="form-group">
+            <label>Mobile</label>
+            <input type="text" name="mobile" value="<?= $doctor['mobile'] ?>">
+        </div>
+    </form>
+   </div>
+</body>
+</html>
