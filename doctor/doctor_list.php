77ac9075af97b09900106c04474b9ae183cff87b
Bayajid35 <bustami22205101235@diu.edu.bd>
2025-12-07 20:20:30 +0600

    Update doctor_list.php

--- doctor/doctor_list.php
+++ doctor/doctor_list.php
@@ -26,4 +26,61 @@ $result = mysqli_query($conn, $query);
         }
     </style>
 </head>
+<body>
+
+<h2>All Doctors List</h2>
+
+<div class="two" style="display: flex;">
+    <div class="download-btns">
+        <a href="export_csv.php" class="btn csv-btn">Download CSV</a>
+    </div>
+    <div class="exit">
+        <a href="doctor.php" class="buttone">EXIT</a>
+    </div>
+</div>
+
+<table class="patient-table">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Doctor ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Mobile</th>
+            <th>Blood Group</th>
+            <th>Sex</th>
+            <th>DOB</th>
+            <th>Designation</th>
+            <th>Department</th>
+            <th>Specialist</th>
+            <th>Status</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+            <tr>
+                <td><?= $row['id'] ?></td>
+                <td><?= $row['doctor_id'] ?></td>
+                <td><?= $row['first_name'] . ' ' . $row['last_name'] ?></td>
+                <td><?= $row['email'] ?></td>
+                <td><?= $row['mobile'] ?></td>
+                <td><?= $row['blood_group'] ?></td>
+                <td><?= $row['sex'] ?></td>
+                <td><?= $row['dob'] ?></td>
+                <td><?= $row['designation'] ?></td>
+                <td><?= $row['department'] ?></td>
+                <td><?= $row['specialist'] ?></td>
+                <td><?= $row['status'] ?></td>
+                <td>
+                    <a class="edit-btn" href="edit_doctor.php?id=<?= $row['id'] ?>">Edit</a> /
+                    <a class="edit-btn" href="delete.php?id=<?= $row['id'] ?>">Delete</a>
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
+</body>    
 </html>
