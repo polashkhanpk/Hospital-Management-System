966d3be446d2ccb5c9bad859486442defdebc25a
Bayajid35 <bustami22205101235@diu.edu.bd>
2025-12-07 20:45:54 +0600

    Update edit_doctor.php

--- doctor/edit_doctor.php
+++ doctor/edit_doctor.php
@@ -1,1 +1,13 @@
+<?php
+session_start();
+include '../Database/connection.php';
 
+$id = $_GET['id'];
+
+
+$query  = "SELECT * FROM doctors WHERE id = '$id'";
+$result = mysqli_query($conn, $query);
+
+if (!$result || mysqli_num_rows($result) != 1) {
+    die("Doctor not found!");
+?>
