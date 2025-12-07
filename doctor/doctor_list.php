e7129b1f0e6d7a5eda99c22cdf6925712c07322b
Bayajid35 <bustami22205101235@diu.edu.bd>
2025-12-07 20:10:01 +0600

    Update doctor_list.php

--- doctor/doctor_list.php
+++ doctor/doctor_list.php
@@ -4,3 +4,26 @@ include '../Database/connection.php';
 $query = "SELECT * FROM doctors ORDER BY id DESC";
 $result = mysqli_query($conn, $query);
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Doctors List</title>
+    <link rel="stylesheet" href="list_style.css">
+    <style>
+        .exit{
+           padding-left: 1600px;
+           padding-bottom: 22px;
+        }
+
+        .buttone{
+            text-decoration: none;
+            background: #ff0000ff;
+            color: white;
+            padding: 5px 10px;
+            text-decoration: none;
+            border-radius: 4px;
+        }
+    </style>
+</head>
+</html>
