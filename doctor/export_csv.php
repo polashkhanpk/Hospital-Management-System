b68bc6c41329c270f497f781dbee749d3966c23b
Bayajid35 <bustami22205101235@diu.edu.bd>
2025-12-08 00:26:42 +0600

    Update export_csv.php

--- doctor/export_csv.php
+++ doctor/export_csv.php
@@ -1,1 +1,20 @@
+<?php
+include '../Database/connection.php';
 
+header('Content-Disposition: attachment; filename="doctor_list.csv"');
+
+$output = fopen("php://output", "w");
+
+fputcsv($output, ['ID', 'Doctor ID', 'First Name', 'Last Name', 'Email', 'Phone no', 'Mobile no', 'Blood Group', 'Sex', 
+                  'DOB', 'Designation', 'Department','Specialist','Address', 'Status']);
+
+$query = "SELECT * FROM doctors";
+$result = mysqli_query($conn, $query);
+
+while ($row = mysqli_fetch_assoc($result)) {
+    fputcsv($output, $row);
+}
+
+fclose($output);
+exit;
+?>
