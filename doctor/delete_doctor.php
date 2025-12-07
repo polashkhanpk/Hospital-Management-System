fef71bdb49f8d00f0b45e0dbea429e006b6e2a09
Bayajid35 <bustami22205101235@diu.edu.bd>
2025-12-07 19:38:47 +0600

    Update delete_doctor.php

--- doctor/delete_doctor.php
+++ doctor/delete_doctor.php
@@ -1,1 +1,17 @@
+<?php
 
+    include '../Database/connection.php';
+
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM doctors WHERE id = '$id'";
+    
+    $run = mysqli_query($conn, $sql);
+
+    if(!$run){
+        echo 'delete operation failed!';
+    } else{
+        header("location: doctor_list.php");
+    }
+
+?>
